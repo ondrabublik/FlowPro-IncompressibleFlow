6d5118854bc4eddc5d9cff37bc7b85822e60bb08
ondrabublik <obublik@kme.zcu.cz>
2020-10-07 09:15:58 +0200

    novy model

--- src/main/java/flowpro/user/equation/IncompressibleNavierStokesSpalartAllmaras.java
+++ src/main/java/flowpro/user/equation/IncompressibleNavierStokesSpalartAllmaras.java
@@ -7,7 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
-public class IncompressibleNavierStokes implements Equation {
+public class IncompressibleNavierStokesSpalartAllmaras implements Equation {
 
     protected class BoundaryType {
 
@@ -43,6 +43,24 @@ public class IncompressibleNavierStokes implements Equation {
     protected double pOut; // pressure
 
     double gravityAcceleration;
+    
+    double vtIn; // turbulence intensity at the inlet
+    
+    // parameters of turbulence model
+    double sigma;
+    double cb1;
+    double cb2;
+    double ka;
+    double cw1;
+    double cw2;
+    double cw3;
+    double cv1;
+    double ct1;
+    double ct2;
+    double ct3;
+    double ct4;
+    double Prt;
+    double C_prod;
 
     @Override
     public int dim() {
@@ -68,7 +86,7 @@ public class IncompressibleNavierStokes implements Equation {
     public void init(FlowProProperties props) throws IOException {
 
         dim = props.getInt("dimension");
-        nEqs = dim + 1;
+        nEqs = dim + 2;
 
         gravityAcceleration = 9.81;
 
@@ -118,6 +136,25 @@ public class IncompressibleNavierStokes implements Equation {
         }
         
         System.out.println("Reynolds number " + Re);
+        
+        // turbulence in inlet
+        vtIn = props.getDouble("vtIn");
+        
+        // parameters of the turbulence model
+        sigma = props.getDouble("sigma");
+        cb1 = props.getDouble("cb1");
+        cb2 = props.getDouble("cb2");
+        ka = props.getDouble("ka");
+        cw1 = cb1 / (ka * ka) + (1 + cb2) / sigma;
+        cw2 = props.getDouble("cw2");
+        cw3 = props.getDouble("cw3");
+        cv1 = props.getDouble("cv1");
+        ct1 = props.getDouble("ct1");
+        ct2 = props.getDouble("ct2");
+        ct3 = props.getDouble("ct3");
+        ct4 = props.getDouble("ct4");
+        Prt = props.getDouble("Prt");
+        C_prod = props.getDouble("C_prod");
     }
 
     @Override
@@ -127,9 +164,9 @@ public class IncompressibleNavierStokes implements Equation {
     @Override
     public double[] constInitCondition() {
         if (dim == 2) {
-            return new double[]{pOut, VIn[0], VIn[1]};
+            return new double[]{pOut, VIn[0], VIn[1], vtIn};
         } else {
-            return new double[]{pOut, VIn[0], VIn[1], VIn[2]};
+            return new double[]{pOut, VIn[0], VIn[1], VIn[2], vtIn};
         }
     }
 
@@ -153,8 +190,8 @@ public class IncompressibleNavierStokes implements Equation {
                     V += elem.meshVelocity[d] * n[d];
                 }
                 f[0] += V;
-                for (int d = 0; d < dim; d++) {
-                    f[d + 1] += V * WR[d + 1];
+                for (int j = 1; j < nEqs; j++) {
+                    f[j] += V * WR[j];
                 }
                 break;
 
@@ -186,6 +223,7 @@ public class IncompressibleNavierStokes implements Equation {
                 double du = VnR - VnL;
                 double us = (VnL + VnR) / 2 - dp / alfa - beta / (2 * alfa) * du;
                 double ps = (WL[0] + WR[0]) / 2 - c2 / alfa * du + beta / (2 * alfa) * dp;
+                double vts = (WL[dim + 1] + WR[dim + 1])/2;
 
                 // tangential velocity
                 double[] vt = new double[dim];
@@ -203,6 +241,7 @@ public class IncompressibleNavierStokes implements Equation {
                 for (int d = 0; d < dim; d++) {
                     Wstar[d + 1] = us * n[d] + vt[d];
                 }
+                Wstar[dim + 1] = vts;
                 f = convectiveFlux(Wstar, n, elem);
                 break;
         }
@@ -223,6 +262,8 @@ public class IncompressibleNavierStokes implements Equation {
         for (int d = 0; d < dim; ++d) {
             f[d + 1] = W[d + 1] * V + W[0] * n[d];
         }
+        f[dim + 1] = W[dim + 1] * V;
+        
         return f;
     }
 
@@ -249,13 +290,25 @@ public class IncompressibleNavierStokes implements Equation {
             }
         }
 
+        double vt = max(0, W[dim + 1]);
+        double[] vtDer = new double[dim];
+        for (int d = 0; d < dim; ++d) {
+            vtDer[d] = dW[d * nEqs + dim + 1];
+        }
+
+        double xi = vt;
+        double fv1 = xi * xi * xi / (xi * xi * xi + cv1 * cv1 * cv1);
+        double mut = rho * vt * fv1; // nejsem si jisty tim rho !!!!!!!!!!!
+        
         double[] flux = new double[nEqs];
         flux[0] = 0;
-        for (int d = 0; d < dim; ++d) {
-            for (int f = 0; f < dim; ++f) {
-                flux[f + 1] += stress[dim * d + f] * n[d] / Re;
+        for (int d = 0; d < dim; d++) {
+            for (int f = 0; f < dim; f++) {
+                flux[f + 1] += (1 + mut) * stress[dim * d + f] * n[d] / Re;
             }
+            flux[dim + 1] += 1 / (Re * sigma) * (1 + vt) * vtDer[d] * n[d];
         }
+        
         return flux;
     }
 
@@ -270,12 +323,50 @@ public class IncompressibleNavierStokes implements Equation {
 
     @Override
     public boolean isSourcePresent() {
-        return false;
+        return true;
     }
 
     @Override
     public double[] sourceTerm(double[] W, double[] dW, ElementData elem) { // zdrojovy clen
-        throw new UnsupportedOperationException("source is not present");
+        
+        double[] velocityJac = new double[dim * dim];
+        for (int d = 0; d < dim; ++d) {
+            for (int f = 0; f < dim; ++f) {
+                velocityJac[dim * d + f] = dW[f * nEqs + d + 1];
+            }
+        }
+
+        double vt = max(0, W[dim + 1]);
+        double vtDerMag = 0;
+        for (int d = 0; d < dim; ++d) {
+            double vtDer = dW[d * nEqs + dim + 1];
+            vtDerMag += vtDer * vtDer;
+        }
+
+        // turbulence limit
+        if (vt < 0) {
+            vt = 0;
+        }
+
+        double D = elem.currentWallDistance;
+
+        double xi = vt; // vt/v 
+        double ft2 = 0; //ct3*Math.exp(-ct4*xi*xi);
+        double ft1 = 0;
+        double fv1 = xi * xi * xi / (xi * xi * xi + cv1 * cv1 * cv1);
+        double fv2 = 1 - xi / (1 + xi * fv1);
+        double Om = rotationMagnitude(velocityJac);
+        double S = Om + 1 / Re * vt / (ka * ka * D * D) * fv2;
+        double rt = vt / (Re * S * ka * ka * D * D);
+        if (rt > 10) {
+            rt = 10;
+        }
+        double g = rt + cw2 * (Math.pow(rt, 6.0) - rt);
+        double fw = g * Math.pow((1 + Math.pow(cw3, 6.0)) / (Math.pow(g, 6.0) + Math.pow(cw3, 6.0)), 1.0 / 6);
+
+        double[] source = new double[nEqs];
+        source[dim + 1] = limitDestruction(1 / Re * cb2 * vtDerMag + cb1 * (1 - ft2) * S * vt - 1 / Re * (cw1 * fw - cb1 / (ka * ka) * ft2) * (vt / D) * (vt / D)); // + r*Re*ft1*dU*dU
+        return source;
     }
 
     @Override
@@ -290,6 +381,7 @@ public class IncompressibleNavierStokes implements Equation {
                     for (int d = 0; d < dim; d++) {
                         WR[d + 1] = u[d];
                     }
+                    WR[dim + 1] = 0;
                 } else {
                     WR[0] = WL[0];
                     double nu = 0;
@@ -299,6 +391,7 @@ public class IncompressibleNavierStokes implements Equation {
                     for (int d = 0; d < dim; ++d) { //tangent to wall
                         WR[d + 1] = WL[d + 1] - n[d] * nu;
                     }
+                    WR[dim + 1] = WL[dim + 1];
                 }
                 break;
             case (BoundaryType.INVISCID_WALL):
@@ -310,6 +403,7 @@ public class IncompressibleNavierStokes implements Equation {
                 for (int d = 0; d < dim; d++) { //tangent to wall
                     WR[d + 1] = WL[d + 1] - n[d] * nu;
                 }
+                WR[dim + 1] = WL[dim + 1];
                 break;
 
             case (BoundaryType.INLET):
@@ -317,6 +411,7 @@ public class IncompressibleNavierStokes implements Equation {
                 for (int d = 0; d < dim; ++d) {
                     WR[d + 1] = VIn[d];
                 }
+                WR[dim + 1] = vtIn;
                 break;
 
             case (BoundaryType.OUTLET):
@@ -324,6 +419,7 @@ public class IncompressibleNavierStokes implements Equation {
                 for (int d = 0; d < dim; ++d) {
                     WR[d + 1] = WL[d + 1];
                 }
+                WR[dim + 1] = WL[dim + 1];
                 break;
         }
         return WR;
@@ -427,8 +523,46 @@ public class IncompressibleNavierStokes implements Equation {
                 }
                 return div;
 
+            case "mut":
+                double xi = max(W[dim + 1],0); // vt/v
+                double fv1 = xi * xi * xi / (xi * xi * xi + cv1 * cv1 * cv1);
+                return new double[]{W[dim + 1]*fv1};    
+                
             default:
                 throw new UnsupportedOperationException("undefined value " + name);
         }
+    }
+    
+    public double max(double a, double b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public double matrixMagnitude(double[] A) {
+        double mag = 0;
+        for (int i = 0; i < A.length; i++) {
+            mag += A[i] * A[i];
+        }
+        return Math.sqrt(mag);
+    }
+
+    public double rotationMagnitude(double[] U) {
+        double rotMag = 0;
+        if (U.length == 4) {
+            rotMag = Math.abs(U[1] - U[2]);
+        } else if (U.length == 9) {
+            rotMag = Math.sqrt((U[0*dim + 1] - U[1*dim + 0]) * (U[0*dim + 1] - U[1*dim + 0]) + (U[0*dim + 2] - U[2*dim + 0]) * (U[0*dim + 2] - U[2*dim + 0]) + (U[2*dim + 1] - U[1*dim + 2]) * (U[2*dim + 1] - U[1*dim + 2]));
+        }
+        return rotMag;
+    }
+
+    public double limitDestruction(double d) {
+        if (d < -100) {
+            d = -100;
+        }
+        return d;
     }
 }
